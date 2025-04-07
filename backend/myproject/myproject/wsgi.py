fbe428d1ec12254c9dd9aff19f7895e649497cca
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-07 10:47:32 +0530

    js

--- backend/myproject/myproject/wsgi.py
+++ backend/myproject/myproject/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Change the settings module path to match your project structure
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
