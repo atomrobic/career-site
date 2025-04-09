12b30acda03384eaad7cdd07489b41d5f87f4b94
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-09 18:41:28 +0530

    hus

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -140,7 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
