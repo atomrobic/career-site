40d493593b451c81b306122f9341352b8f222ae6
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-10 13:57:18 +0530

    ki

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 
     # Third-party apps
     'rest_framework',
