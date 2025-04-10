88e57fdbec185c654f281cf6289a330ea80a4f61
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-04-10 16:48:18 +0530

    ki

--- frontend/my-react-app/vite.config.js
+++ frontend/my-react-app/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,  // Ensure this matches your frontend port
-  },
   build: {
-    outDir: "dist", // Ensure this matches Netlify’s publish directory
-  },
-});
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
+})
