c84422970b7d49c4926ff77a54d88262ac0bef3e
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-05-03 13:24:23 +0530

    adding

--- frontend/my-react-app/src/Approuter.jsx
+++ frontend/my-react-app/src/Approuter.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login, Register, OtpVerification } from "./components/AuthMain";
+
 import Home from "./components/Home";
 import JobDetails from "./components/JobDetails";
- import AdminDashboard from "./components/Admin/AdminDashboard";
+//  import AdminDashboard from "./components/AuthMain";
 // import ApplyJob from "./pages/ApplyJob";
-import Login from "./components/Login";
 import Profile from "./components/Profile";
 // import NotFound from "./pages/NotFound";
 
@@ -13,10 +14,12 @@ function AppRoutes() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/details/:id" element={<JobDetails />} />
-        <Route path="/"element={<Login/>}/>
-      
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/verify-otp" element={<OtpVerification />} />      
         <Route path="/profile"element={<Profile/>}/>
-        <Route path="/AdminDashboard"element={< AdminDashboard/>}/>
+
+        {/* <Route path="/AdminDashboard"element={< AdminDashboard/>}/> */}
       </Routes>
     </Router>
   );
